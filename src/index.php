41b6ced00422541aa9077db0af93e2f6322ffc2f
Ademar-Rafhael <vnarafha@gmail.com>
2024-09-03 16:36:37 -0300

    commit: Add processing to functionalities

--- src/index.php
+++ src/index.php
@@ -9,24 +9,59 @@
   <body>
 
     <div class="" >
-      <form method="POST" action="connection.php" class = "p-4 ">
-        <div class="mb-4">
-          <label for="exampleInputEmail1" class="form-label">Email address</label>
-          <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+      <nav class="navbar navbar-expand-lg bg-body-tertiary">
+        <div class="container-fluid">
+          <a class="navbar-brand" href="#">Tecnotel</a>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+              <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="index.php">Home</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="?page=registro">Cadastro</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="?page=listar">Usuários</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="#">Sobre</a>
+              </li>
+            </ul>
+          </div>
         </div>
-        <div class="mb-3">
-          <label for="exampleInputPassword1" class="form-label">Password</label>
-          <input type="password" class="form-control" id="exampleInputPassword1">
-        </div>
-        <div class="mb-3 form-check">
-          <input type="checkbox" class="form-check-input" id="exampleCheck1">
-          <label class="form-check-label" for="exampleCheck1">Check me out</label>
-        </div>
-        <button type="submit" class="btn btn-primary">Submit</button>
-      </form>
+      </nav>
     </div>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+
+    <div class="container">
+      <div class="row">
+        <div class="col mt-5">
+          
+          <?php 
+              include("connection.php");
+              switch (@$_REQUEST["page"]) {
+              case 'registro':
+                include("cadastro.php");
+                break;
+              
+              case 'listar':
+                include("users.php");
+                break;
+
+              default:
+                print "Bem Vindos";
+                break;
+            }
+          ?>
+
+        </div>
+      </div>
+    </div>
+
 
   </body>
 </html>
